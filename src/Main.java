43a2a6a514b90f90c4a8564c838224f03aa322f8
Jaum_1502 <aquelecaraalto@gmail.com>
2024-06-13 00:39:56 -0300

    ponho

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public Main() {
         Scanner entrada = new Scanner(System.in);
-        int[][] matriz = new int[8][8];
+        int[][] matriz1 = new int[8][8];
+        int[][] matriz2 = new int[8][8];
         System.out.println("Digite o tipo de barco que voce deseja colocar: ");
         System.out.println("1.  Barco Pequeno (1 espaço)");
         System.out.println("2.  Barco Médio   (2 espaço)");
@@ -11,30 +12,31 @@ public class Main {
         int tipoBarco = tipoBarco(entrada);
         int linha = linha(entrada);
         int coluna = coluna(entrada);
-        definindoCasas(entrada, matriz);
+        definindoCasas(entrada, matriz1);
         switch (tipoBarco) {
             case 1:
                 for (int i = 0; i < 3; i++) {
-                    barco1(matriz, linha, coluna, tipoBarco);
+                    barco1(matriz1, linha, coluna, tipoBarco);
                 }
                 break;
 
             case 2:
                 for (int i = 0; i < 3; i++) {
-                    barco2(matriz, linha, coluna, tipoBarco);
+                    barco2(matriz1, linha, coluna, tipoBarco);
                 }
                 break;
 
             case 3:
                 for (int i = 0; i < 3; i++) {
-                    barco3(matriz, linha, coluna, tipoBarco);
+                    barco3(matriz1, linha, coluna, tipoBarco);
                 }
                 break;
 
             default:
                 break;
         }
-        impressaoMatrizes(matriz);
+        espacoBranco();
+        impressaoMatriz1es(matriz1, matriz2);
 
     }
   // ----------------------------------------------------ENTRADAS NAVIOS----------------------------------------------------
@@ -60,51 +62,70 @@ public class Main {
 
 
     // ----------------------------------------------------DENIFINDO CASAS----------------------------------------------------
-    public void definindoCasas(Scanner entrada, int[][] matriz) {
+    public void definindoCasas(Scanner entrada, int[][] matriz1) {
 
-        for (int i = 0; i < matriz.length; i++)
+        for (int i = 0; i < matriz1.length; i++)
 
         {
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = 0;
+            for (int j = 0; j < matriz1[i].length; j++) {
+                matriz1[i][j] = 0;
             }
         }
     }
 
-    // ----------------------------------------------------IMPRESSAO MATRIZES----------------------------------------------------
-    public void impressaoMatrizes(int matriz[][]) {
-        for (int i = 0; i < matriz.length; i++)
+    // ----------------------------------------------------IMPRESSAO MATRIZ1ES----------------------------------------------------
+
+    public void impressaoMatriz1es(int matriz1[][], int matriz2[][]) {
+        for (int i = 0; i < matriz1.length; i++)
 
         {
-            for (int j = 0; j < matriz[i].length; j++) {
+            for (int j = 0; j < matriz1[i].length; j++) {
 
-                System.out.print(matriz[i][j] + " ");
+                System.out.print(matriz1[i][j] + " ");
+
+            }
+            System.out.print("              ");
+            for (int j = 0; j < matriz2[i].length; j++) {
+
+                System.out.print(matriz2[i][j] + " ");
 
             }
             System.out.println();
         }
     }
 
-    public void barco1(int matriz[][], int linha, int coluna, int tipoBarco) {
+    public void barco1(int matriz1[][], int linha, int coluna, int tipoBarco) {
         if (tipoBarco == 1) {
-            matriz[linha][coluna] = 1;
+            matriz1[linha][coluna] = 1;
         }
     }
 
-    // -------------------
-    public void barco2(int matriz[][], int linha, int coluna, int tipoBarco) {
+    public void barco2(int matriz1[][], int linha, int coluna, int tipoBarco) {
         if (tipoBarco == 2) {
-            matriz[linha][coluna] = 1;
-            matriz[linha][coluna + 1] = 1;
+            matriz1[linha][coluna] = 1;
+            matriz1[linha][coluna + 1] = 1;
         }
     }
 
-    public void barco3(int matriz[][], int linha, int coluna, int tipoBarco) {
+    public void barco3(int matriz1[][], int linha, int coluna, int tipoBarco) {
         if (tipoBarco == 3) {
-            matriz[linha][coluna] = 1;
-            matriz[linha][coluna + 1] = 1;
-            matriz[linha][coluna + 2] = 1;
+            matriz1[linha][coluna] = 1;
+            matriz1[linha][coluna + 1] = 1;
+            matriz1[linha][coluna + 2] = 1;
         }
+    }
+
+    public void espacoBranco(){
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
 
     public static void main(String[] args) {
